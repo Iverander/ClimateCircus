1241e27b652b2ff13f98ae1b93cd2ba8d234dce3
AngelinaGroiss <e12323841@student.tuwien.ac.at>
2026-03-24 15:35:54 +0100

    starting ringmaster scene
    starting ringmaster scene, creating moving rods with tree for the minigame

---
+++ Assets/!Scripts/MovingRod.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+
+
+public class MovingRod : MonoBehaviour
+{
+ public float amplitude = 0.2f;
+    public float speed = 2f;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        float offset = Mathf.Sin(Time.time * speed) * amplitude;
+        transform.position = startPos + transform.up * offset;
+    }
+}
