c170a509c740bd4812338cdded4d0ee3772e8e0a
Iverander <torje@iverander.com>
2026-04-10 18:20:55 +0200

    waterballoon should stay lol

---
+++ Assets/!Scripts/waterBaloon.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+// Simple grabable object with no extra effects
+// A simple grabable with no effects can be achieved with just the grabable script :)))
+public class waterBaloon : Grabable // a monobehaviour script (a script thats put onto objects) has to have the same name as its file btw :)))
+{
+
+}
